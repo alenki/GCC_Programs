2c8190b826992607aec059b32bb965f66e4c8425
Alenki <63051978+alenki@users.noreply.github.com>
2022-10-13 13:18:13 +0300

    ...

---
+++ main2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	unsigned int h(0), n(0), INF(1e6), ans1(0), ans2(INF), h1(INF), i1(0), h2(INF), i2(0);
+	cin >> n;
+	for(int i = 1; i < (n+1); i++)
+	{
+		cin >> h;
+		if(h==h2)
+		{
+			i2 = i;
+		}
+		else
+		{
+			if (h1 < h2 && h2 > h && i - i1 < ans2 - ans1)
+			{
+				ans1 = i1;
+				ans2 = i;
+			}
+			h1 = h2;
+			i1 = i2;
+			h2 = h;
+			i2 = i;
+		}
+	}
+	if (ans1 == 0) cout << "0";
+	else cout << ans1 << ans2;
+}
+
